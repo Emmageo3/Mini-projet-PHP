741d4940646b310e58b14c5e146133691addd3f2
Emmageo3 <kanfanyemma22@gmail.com>
2022-02-14 13:30:04 +0000

    page accueil

--- connection-db.php
+++ connection-db.php
@@ -1,16 +1,17 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'simplon');
- 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
+
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'simplon');
+    
+    
+    $bdd = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    
+    // verifier la connexion
+    if($bdd === false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }else{
+        echo 'Je suis bien connectée a la base de données';
+    }
 ?>
